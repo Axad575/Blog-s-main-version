a3ad1a1f2f7ec92c46ed213c4bdb0423c7471257
Axad575 <112960400+Axad575@users.noreply.github.com>
2024-06-19 18:29:19 +0500

    update #3

--- src/app/page.js
+++ src/app/page.js
@@ -9,33 +9,7 @@ import { MdSearch } from "react-icons/md";
 import { useState, useEffect } from 'react';
 
 export default function Home() {
-
-//   // const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://abdulaxad575:Abdu1axad2839@firstexample.ipcbbsn.mongodb.net/?retryWrites=true&w=majority&appName=FirstExample";
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-
-
+  
 const [task, setTask] = useState('');
   const [tasksArray, setTasksArray] = useState([]);
 
@@ -80,18 +54,7 @@ const [task, setTask] = useState('');
     <div>
       <div id={styles.FirstDiv}>
         <h1 id={styles.LogoText}>Blog's</h1>
-        <div id={styles.IconDiv}>
-        <Link id={styles.LinkColor} href={"../pages/explore"}>
-          <MdExplore id={styles.ExploreIcon} />
-        </Link>
-        <Link id={styles.LinkColor} href={"../pages/my-account"}>
-        <MdAccountCircle id={styles.AccountIcon} />
-        </Link>
-        <MdQueueMusic id={styles.TranslateIcon} />
-        <Link id={styles.LinkColor} href="">
-        <MdSearch id={styles.SearchIcon}/>
-        </Link>
-        </div>
+        
       </div>
       <div id={styles.MainText}>
         <h1>What are you thinking about?</h1>
@@ -99,10 +62,11 @@ const [task, setTask] = useState('');
           <input id={styles.MainInput}  value={task} onChange={inputChange} onKeyPress={handleKeyPress} placeholder="Write here"></input>
           <button id={styles.UploadButton}>Upload</button>
         </form>
+        <p id={styles.save}>They won't save in our service</p>
         <div id={styles.CardBox}>
         {tasksArray.map((task, index) => (
           <div id={styles.CardDiv} key={index}>
-            <h1 id={styles.CardName}>You</h1>
+            <h1 id={styles.CardName}>You blog</h1>
             <h1 id={styles.CardValue}>{task}</h1>
             <h3 id={styles.CardDayTime}>{currentDay}, {currentTime}</h3>
           </div>
